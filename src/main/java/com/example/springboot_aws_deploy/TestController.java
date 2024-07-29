3addbb646574799ecf2e41263c025e5eb0e7de26
VenkataNaresh R <rvnaresh61@gmail.com>
2024-07-29 18:46:36 -0400

    modified files

--- src/main/java/com/example/springboot_aws_deploy/TestController.java
+++ src/main/java/com/example/springboot_aws_deploy/TestController.java
@@ -1,4 +1,4 @@
-package com.naresh.awsdeploy.controller;
+package com.example.springboot_aws_deploy;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
