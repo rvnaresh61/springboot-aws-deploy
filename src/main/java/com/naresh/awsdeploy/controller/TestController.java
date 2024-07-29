87b6bbaa044242cc0388a05caa561e0db2eaa589
VenkataNaresh R <rvnaresh61@gmail.com>
2024-07-29 18:35:53 -0400

    fourth commit

---
+++ src/main/java/com/naresh/awsdeploy/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.naresh.awsdeploy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/aws")
+public class TestController {
+	
+	@GetMapping("/test")
+	public String awsDeploymentTest() {
+		return "Hi Naresh in ECS Deploymemt is Completed";
+	}
+
+}
